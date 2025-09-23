f72b8dd55515da4a12412e79c38e534b16b0e3a4
ROBERTTest1 <robi.tarlap123@gmail.com>
2025-09-23 10:37:32 +0300

    DBTables

--- ShopTARge24/ShopTARge24.Core/Domain/Kindergarten.cs
+++ ShopTARge24/ShopTARge24.Core/Domain/Kindergarten.cs
@@ -14,6 +14,6 @@ namespace ShopTARge24.Core.Domain
         public string? KindergartenName { get; set; }
         public string? TeacherName { get; set; }
         public DateTime? CreatedAt { get; set; }
-        public DateTime? ModifiedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
