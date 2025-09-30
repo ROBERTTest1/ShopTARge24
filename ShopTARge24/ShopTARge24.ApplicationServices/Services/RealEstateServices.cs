00028f16fca1b62eac8f5c6e54761fe48363614f
MSI\ingva <inde999@gmail.com>
2025-09-30 11:09:55 +0300

    RealEstateService

--- ShopTARge24/ShopTARge24.ApplicationServices/Services/RealEstateServices.cs
+++ ShopTARge24/ShopTARge24.ApplicationServices/Services/RealEstateServices.cs
@@ -1,9 +1,77 @@
-﻿using ShopTARge24.Core.ServiceInterface;
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARge24.Core.Domain;
+using ShopTARge24.Core.Dto;
+using ShopTARge24.Core.ServiceInterface;
+using ShopTARge24.Data;
+
 
 namespace ShopTARge24.ApplicationServices.Services
 {
     public class RealEstateServices : IRealEstateServices
     {
+        private readonly ShopTARge24Context _context;
 
+        public RealEstateServices
+            (
+                ShopTARge24Context context
+            )
+        {
+            _context = context;
+        }
+
+        public async Task<RealEstate> Create(RealEstateDto dto)
+        {
+            RealEstate domain = new RealEstate();
+
+            domain.Id = dto.Id;
+            domain.Area = dto.Area;
+            domain.Location = dto.Location;
+            domain.RoomNumber = dto.RoomNumber;
+            domain.BuildingType = dto.BuildingType;
+            domain.CreatedAt = DateTime.Now;
+            domain.ModifiedAt = DateTime.Now;
+
+            await _context.RealEstates.AddAsync(domain);
+            await _context.SaveChangesAsync();
+
+            return domain;
+        }
+
+        public async Task<RealEstate> Update(RealEstateDto dto)
+        {
+            RealEstate domain = new RealEstate();
+
+            domain.Id = dto.Id;
+            domain.Area = dto.Area;
+            domain.Location = dto.Location;
+            domain.RoomNumber = dto.RoomNumber;
+            domain.BuildingType = dto.BuildingType;
+            domain.CreatedAt = DateTime.Now;
+            domain.ModifiedAt = DateTime.Now;
+
+            _context.RealEstates.Update(domain);
+            await _context.SaveChangesAsync();
+
+            return domain;  
+        }
+
+        public async Task<RealEstate> DetailAsync(Guid id)
+        {
+            var result = await _context.RealEstates
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return result;
+        }
+
+        public async Task<RealEstate> Delete(Guid id)
+        {
+            var result = await _context.RealEstates
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            _context.RealEstates.Remove(result);
+            await _context.SaveChangesAsync();
+
+            return result;
+        }
     }
 }
